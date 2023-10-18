667e557fc7712b888b7a95ab3ae8ccab2d232746
yutfut <seregey.golubev@mail.ru>
2023-10-18 23:57:47 +0300

    added files flags

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "io"
     "fmt"
+    "flag"
     "log"
     "time"
     "os"
@@ -33,8 +34,7 @@ func readerLog(wg *sync.WaitGroup, file *os.File, che *ch.ClickHouse) {
 
     for {
         line, _, err := reader.ReadLine()
-        
-        if err == io.EOF {
+        if err != nil && err != io.EOF {
             fmt.Println(err)
         }
 
@@ -45,26 +45,57 @@ func readerLog(wg *sync.WaitGroup, file *os.File, che *ch.ClickHouse) {
     }
 }
 
+
+// https://stackoverflow.com/questions/28322997/how-to-get-a-list-of-values-into-a-flag-in-golang
+
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+    return "my string representation"
+}
+
+func (i *arrayFlags) Set(value string) error {
+    *i = append(*i, value)
+    return nil
+}
+
+var files arrayFlags
+
 func main() {
+    flag.Var(&files, "file", "go run ./main.go -file file.log -file file1.log -file file2.log")
+    flag.Parse()
+
+    fmt.Println(files)
+
     conn, err := ch.Connect()
     if err != nil {
         panic((err))
     }
 
-    che := ch.NewClickHouse(conn)
-
-    f, err := os.OpenFile("file.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer f.Close()
+    clickHouseDriver := ch.NewClickHouse(conn)
 
     wg := &sync.WaitGroup{}
-	fmt.Println("hello")
 
-    wg.Add(2)
-    go writeLog(wg, f)
+    for _, file := range files {
 
-    go readerLog(wg, f, che)
+        fileInput, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+        if err != nil {
+            log.Fatalf("error opening file: %v", err)
+        }
+        defer fileInput.Close()
+    
+        fileOutput, err := os.Open(file)
+        if err != nil {
+            log.Fatalf("error opening file: %v", err)
+        }
+        defer fileOutput.Close()
+
+        wg.Add(2)
+    
+        go writeLog(wg, fileInput)
+    
+        go readerLog(wg, fileOutput, clickHouseDriver)
+    }
+
     wg.Wait()
 }
